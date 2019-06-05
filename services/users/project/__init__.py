460af3f680f5e95184f69e1a7beb02b4322df94d
Jeff <yzha2577@uwo.ca>
2019-06-05 12:18:42 -0500

    Added React FrontEnd

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,14 +1,15 @@
-# services/users/project/__init__.py
-
-
 import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_cors import CORS  # new
 
 
-# instantiate the db
+# instantiate the extensions
 db = SQLAlchemy()
+toolbar = DebugToolbarExtension()
+cors = CORS()  # new
 
 
 def create_app(script_info=None):
@@ -22,6 +23,8 @@ def create_app(script_info=None):
 
     # set up extensions
     db.init_app(app)
+    toolbar.init_app(app)
+    cors.init_app(app)  # new
 
     # register blueprints
     from project.api.users import users_blueprint
